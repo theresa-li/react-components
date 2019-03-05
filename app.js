15a82cf926fe27f5f5c71c9f51be73175584e600
Theresa <tli2@cca.edu>
2019-03-04 22:49:39 -0800

    Create React components with props

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceries={['Cucumbers', 'Kale']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    <li>{props.groceries[0]}</li>
+    <li>{props.groceries[1]}</li>
+  </ul>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
